cb9bc1bf65dcc41fe24032f6313c1fd466fee866
Bohdana Turcheniak <bogdanaturcheniak@gmail.com>
2026-04-15 23:16:04 +0300

    almost-done

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,11 @@
+const refs = {
+  backgroundColor: document.querySelector('.color'),
+  btnChangeColor: document.querySelector('.change-color'),
+};
+
+refs.btnChangeColor.addEventListener('click', getRandomHexColor);
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const color = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
