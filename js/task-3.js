a876f6b5f0d7e2713e3d58d40fcc99ea232a80bf
Bohdana Turcheniak <bogdanaturcheniak@gmail.com>
2026-04-15 15:32:22 +0300

    done-task-3

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+const refs = {
+  nameInput: document.querySelector('#name-input'),
+  nameOutput: document.querySelector('#name-output'),
+};
+refs.nameInput.addEventListener('input', event => {
+  const inputText = refs.nameInput.value.trim();
+  if (inputText === '') {
+    refs.nameOutput.textContent = 'Anonymous';
+  } else {
+    refs.nameOutput.textContent = refs.nameInput.value;
+  }
+});
