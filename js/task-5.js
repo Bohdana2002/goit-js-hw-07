af4191d19e20a0b51948c3bd0eb7c03e0a965fd1
Bohdana Turcheniak <bogdanaturcheniak@gmail.com>
2026-04-16 09:34:21 +0300

    done-hometask-7

--- js/task-5.js
+++ js/task-5.js
@@ -8,4 +8,7 @@ function getRandomHexColor() {
   const color = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+
+  document.body.style.backgroundColor = color;
+  refs.backgroundColor.textContent = color;
 }
